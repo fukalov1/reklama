29e9653c4f3121a8edfaa7a6c2ed718fcad0305b
fai <fai_@mail.ru>
2019-02-26 07:53:22 +0400

    version 1.0 генерирует заголовки объявлений и экспортирует результат в HTML and Excel

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -11,7 +11,7 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link" data-toggle="tab" href="#tab_ext" :class="{active : current_tab==3}"
-                   @click="setTab('3')">Результат</a>
+                   @click="setTab('3')">Результат HTML</a>
             </li>
         </ul>
         <div class="tab-content">
@@ -42,18 +42,21 @@
                     </div>
                     <div class="col-3 col-lg-3">
                         <h6>Данные</h6>
-                        <textarea v-model="text" class="form-control"
+                        <textarea v-model="text" class="form-control"cols="100" rows="6"
                                   placeholder="Вставьте текст с фразами для генерации объявлений. Каждая фраза с новой строки. После введите ключевые слова для заголовков и намите кнопку сгенерировать"></textarea>
 
                     </div>
                     <div class="col-3 col-lg-3">
                         <div class="row">
                             <div class="col-lg-6">
-                                <button type="button" class="btn btn-primary" @click="generateHeaders">сгенерировать
+                                <button type="button" class="btn btn-primary" @click="generateHeaders">
+                                    получить Заголовок 1
                                 </button>
                             </div>
                             <div class="col-lg-6">
-
+                                <button type="button" class="btn btn-primary" @click="generateSecondHeaders">
+                                    получить Заголовок 2
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -72,25 +75,25 @@
                     </div>
                     <div class="col-lg-4">
                         <h6>Заголовок 1 (max 35)</h6>
-                        <div class="row" v-for="(header_word, key) in header1">
+                        <div class="row" v-for="(header_word) in header1">
                             <div class="col-lg-11">
                                 <input type="text" v-model="header_word.value" class="custom-text"
-                                       :class="{danger: header_word.value.length>35}" @change="test(header_word.value, key)"/>
+                                       :class="{danger: getLength(header_word.value)>35}"/>
                             </div>
                             <div class="col-lg-1">
-                                <span> {{ header_word.value.length }}</span>
+                                <span> {{ getLength(header_word.value) }}</span>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-5">
                         <h6>Заголовок 2 (max 53)</h6>
-                        <div class="row" v-for="(header2_word) in header2">
+                        <div class="row" v-for="(header2_word, key) in header2">
                             <div class="col-lg-11">
                                 <input type="text" v-model="header2_word.value" class="custom-text"
-                                       :class="{danger: header2_word.value.length>53}"/>
+                                       :class="{danger: (getLength(header1[key].value)+getLength(header2_word.value))>53}"/>
                             </div>
                             <div class="col-lg-1">
-                                <span> {{ header2_word.value.length }}</span>
+                                <span> {{ (getLength(header1[key].value)+getLength(header2_word.value)) }}</span>
                             </div>
                         </div>
                     </div>
@@ -111,17 +114,17 @@
                         <input type="text" v-model="a1" class="form-control"placeholder="хвост к метке"/>
                     </div>
                     <div class="col-4 col-lg-4">
-                        <label>Текст ({{ info_text.length }})</label>
-                        <textarea v-model="info_text" class="form-control" :class="{danger: info_text.length>81}" placeholder="Дополнительный текст не более 81 символа"></textarea>
+                        <label>Текст ({{ getLength(info_text) }})</label>
+                        <textarea v-model="info_text" class="form-control" :class="{danger: getLength(info_text)>81}" placeholder="Дополнительный текст не более 81 символа"></textarea>
                     </div>
                     <div class="col-2 col-lg-2">
                         <br/>
                         <button type="button" class="btn btn-success" @click="generateHTML">экспорт в HTML</button><br/><br/>
-                        <button type="button" class="btn btn-danger">экспорт в Excel</button>
                     </div>
                 </div>
             </div>
             <div class="tab-pane" :class="{active : current_tab==3}">
+                <div class="col-lg-12 text-right"> <button type="button" class="btn btn-danger" @click="exportExcel">экспорт Excel</button></div>
                 <h3>Предложение текстовых блоков для рекламной кампании</h3>
                 <table border="1">
                     <tr v-for="(item, key) in result_html">
@@ -140,17 +143,25 @@
 </template>
 
 <script>
+
+    import axios from 'axios';
+
     export default {
         data: function () {
             return {
-                words: [{'value': 'первое', 'count': ''}, {'value': 'второе', 'count': ''}, {
-                    'value': 'третье',
-                    'count': ''
-                }, {'value': 'четвертое', 'count': ''}, {'value': 'пятое', 'count': ''}],
-                second_words: [{'value': 'первое', 'count': ''}, {'value': 'второе', 'count': ''}, {
-                    'value': 'третье',
-                    'count': ''
-                }, {'value': 'четвертое', 'count': ''}, {'value': 'пятое', 'count': ''}],
+                words: [
+                    {'id': '0', 'value': 'купить', 'count': ''},
+                    {'id': '1', 'value': 'акция', 'count': ''},
+                    {'id': '2', 'value': '25!', 'count': ''},
+                    {'id': '3', 'value': '', 'count': ''},
+                    {'id': '4', 'value': '', 'count': ''}
+                    ],
+                second_words: [
+                    {'id': '0', 'value': 'даром', 'count': ''},
+                    {'id': '1', 'value': '13!', 'count': ''},
+                    {'id': '2', 'value': 'бесплатно', 'count': ''},
+                    {'id': '3', 'value': '', 'count': ''},
+                    {'id': '4', 'value': '', 'count': ''}],
                 minus_words: [],
                 header1: [],
                 header2: [],
@@ -166,15 +177,17 @@
                 s1: '?utm_source=Yandex&utm_medium=Direct&utm_term={Keyword}&utm_content=Poisk&utm_campaign=',
                 a1: '',
                 group_name: '',
-                result_html: []
+                result_html: [],
+                length_header1: 35,
+                length_header2: 53
             }
         },
         created() {
-            for (let i = 0; i <= this.count_header; i++) {
-                this.minus_words.push({'value': ''});
-                this.header1.push({'value': ''});
-                this.header2.push({'value': ''});
-            }
+            // for (let i = 0; i <= this.count_header; i++) {
+            //     this.minus_words.push({'value': ''});
+            //     this.header1.push({'value': ''});
+            //     this.header2.push({'value': ''});
+            // }
         },
         watch: {
             count_header: function (val) {
@@ -201,6 +214,20 @@
             },
         },
         methods: {
+            getLength(val) {
+                val = val.replace(/\s+/g,' ');
+                val = val.replace(/\++/g,' ');
+                val = val.replace(/\_+/g,' ');
+                val = val.replace(/\%+/g,'');
+                val = val.replace(/\!+/g,'');
+                val = val.replace(/\?+/g,'');
+                val = val.replace(/\#+/g,'');
+                val = val.replace(/\"+/g,'');
+                val = val.replace(/\$+/g,'');
+                val = val.replace(/\,+/g,'');
+                val = val.replace(/\.+/g,'');
+                return val.length;
+            },
             test(val, key) {
                 console.log('change header 2', key, val);
                 this.header2[key].value = val +' '+ this.second_words[this.randomInteger(0, 4)].value;
@@ -215,28 +242,96 @@
                 //     console.log('val', item, i);
                 // })
                 this.header1 = this.minus_words.map((item, i) => {
-                    return {'value': this.getWord(item.value)};
+                    return {'value': this.getWord(item.value, this.words, this.length_header1)};
                 });
-                console.log(this.header1);
+                // console.log(this.header1);
 
+                // this.header2 = this.header1.map((item, i) => {
+                //     return {'value': this.getSecondWord()};
+                // });
+            },
+            generateSecondHeaders: function () {
+                console.log('generate headers2');
+                this.process = true;
                 this.header2 = this.header1.map((item, i) => {
-                    return {'value': this.getSecondWord(item.value)};
+                    return {'value': this.getSecondWord(item.value, this.second_words, this.length_header2)};
                 });
             },
-            getWord(word) {
+            getWord(word, words, max_length) {
                 let result = ''
                 if (word != '') {
                     word = word.charAt(0).toUpperCase() + word.substr(1);
-                    word = word.replace(/\+/g,' ');
-                    word = word.replace(/\_/g,' ');
-                    result = word + ' ' + this.words[this.randomInteger(0, 4)].value;
+                    word = word.replace(/\++/g,' ');
+                    word = word.replace(/\_+/g,' ');
+                    let temp_words = Object.assign(words);
+                    temp_words = temp_words.filter(function (val) {
+                        // console.log('current word', val);
+                        return val.value.length>0;
+                    });
+                    // console.log('temp words', temp_words);
+
+                    let count = temp_words.length;
+                    for(let i=0; i< count; i++) {
+                        let max_word = temp_words.reduce(function (item, current) {
+                            // console.log('item', item, current);
+                            if (item.value.length < current.value.length)
+                                item = current;
+                            return item;
+                        });
+                        result = word + ' ' + max_word.value;
+                        console.log(word, 'max word', max_word.value,'result', result.length);
+                        if (result.length>=max_length) {
+                            temp_words = temp_words.filter(function (val) {
+                                return val.value!=max_word.value;
+                            });
+                            // console.log('delete long word',max_word.value, temp_words);
+                            // console.log('temp_words', temp_words);
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    // result = word + ' ' + this.words[this.randomInteger(0, 4)].value;
                 }
                 return result
             },
-            getSecondWord(word) {
+            getSecondWord(word, words, max_length) {
                 let result = ''
-                if (word != '')
-                    result = word + ' ' + this.second_words[this.randomInteger(0, 4)].value;
+                if (word != '') {
+                    word = word.charAt(0).toUpperCase() + word.substr(1);
+                    word = word.replace(/\++/g,' ');
+                    word = word.replace(/\_+/g,' ');
+                    let temp_words = Object.assign(words);
+                    temp_words = temp_words.filter(function (val) {
+                        // console.log('current word', val);
+                        return val.value.length>0;
+                    });
+                    // console.log('temp words', temp_words);
+
+                    let count = temp_words.length;
+                    for(let i=0; i< count; i++) {
+                        let max_word = temp_words.reduce(function (item, current) {
+                            // console.log('item', item, current);
+                            if (item.value.length < current.value.length)
+                                item = current;
+                            return item;
+                        });
+                        result = max_word.value;
+                        console.log(word, 'max word', max_word.value,'result', result.length);
+                        if ((word + ' ' + max_word.value).length>=max_length) {
+                            temp_words = temp_words.filter(function (val) {
+                                return val.value!=max_word.value;
+                            });
+                            result = max_word.value;
+                            // console.log('delete long word',max_word.value, temp_words);
+                            // console.log('temp_words', temp_words);
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    // result = word + ' ' + this.words[this.randomInteger(0, 4)].value;
+                }
                 return result
             },
             randomInteger(min, max) {
@@ -261,7 +356,25 @@
                         'info_text': this.info_text,
                         'url': this.url+this.s1+this.a1}
                         );
-                })
+                });
+                this.current_tab = 3;
+                this.exportExcel();
+            },
+            exportExcel() {
+                console.log('Start export data to Excel');
+
+                axios.post('/load_data.php', {
+                    data: JSON.stringify(this.result_html)
+                }).then(response => {
+                    console.log('response', response.data);
+                    let data = response.data;
+                    console.log('data', data,data.filename);
+                    if (data.filename != '') {
+                        window.open('/files/'+data.filename,'_blank');
+                    }
+                }).catch(function (error) {
+                    console.log('error load  export data', error);
+                });
             }
 
         }
@@ -292,6 +405,7 @@
 
     textarea {
         height: 190px;
+
     }
     table {
         width: 100%;
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
